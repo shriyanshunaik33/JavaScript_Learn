fb39a830f38ae09a28241d34b6cdd2d8bdb54208
shriyanshunaik33 <shriyanshunaik.33@gmail.com>
2026-01-03 17:44:22 +0000

    variables and datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountID = 121212
+let accountEmail = "Hello@gmail.com"
+var accountPass = "sdfjsn"
+accountCity = "Pune"
+let accountState
+//accountID = 2423 (Not possible)
+
+console.table([accountID,accountEmail,accountPass,accountCity,accountState])
+
+// prefer not to use var, because of issue in block scope and functional scope
---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,19 @@
+"use strict" //treat all JS code as newer version
+
+//alert(3+3) this cant be used on Nodejs, it was used on browerser
+
+// Number
+// BigInt
+// String
+// Boolean
+// null (standalone value)
+// undefined
+// symbol (Unique)
+
+
+
+// Object
+
+
+console.log(typeof undefined)   //undefined
+console.log(typeof null)        //onject
