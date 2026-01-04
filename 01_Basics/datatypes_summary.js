7188696b8b04b911e98d75ec1df475ae42e310f5
shriyanshunaik33 <shriyanshunaik.33@gmail.com>
2026-01-04 08:33:07 +0000

    datatype summary

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,11 @@
+// Primitive
+
+    //  7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+// Reference (Non Primituve)
+
+    // Array, Objects, Functions
+
+    // https://262.ecma-international.org/5.1/#sec-11.4.3
+    
