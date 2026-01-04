9a6097e231ba0a414d4b54137c5fe48f0e829f84
shriyanshunaik33 <shriyanshunaik.33@gmail.com>
2026-01-04 08:58:36 +0000

    memory in JS

--- 01_Basics/datatypes_summary.js
+++ 01_Basics/datatypes_summary.js
@@ -9,3 +9,31 @@
 
     // https://262.ecma-international.org/5.1/#sec-11.4.3
     
+
+// ****************************************************************
+// Memory in JavaScript
+
+// Stack (Premetive)
+
+let name1 = "shriyanshu"
+
+let name2 = name1
+name2 = "Naik" // it gets the copy of the original which is changed here
+
+console.log(name1)
+console.log(name2)
+
+// Heap (Non-Prementive)
+
+let userOne={
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email ="shriyasnhu@gmail.com" // this changes the reference itself
+
+console.log(userOne)
+console.log(userTwo)
+
